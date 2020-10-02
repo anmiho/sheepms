48a25fe64b2341bfcf6446ceed3d995e29bcece5
Anmiho <1307426250@qq.com>
2020-10-02 14:08:20 +0800

    创建项目

---
+++ src/main/java/com/mian/sheepms/SheepmsApplication.java
@@ -0,0 +1,13 @@
+package com.mian.sheepms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheepmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheepmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mian/sheepms/SheepmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mian.sheepms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheepmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
